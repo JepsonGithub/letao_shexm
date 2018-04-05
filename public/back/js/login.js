709c2fa5fb838fe061231154af818bdec3f83490
jepsongithub <jepsonpp@qq.com>
2018-04-06 00:23:14 +0800

    login: 请求登陆功能

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -48,4 +48,37 @@ $(function() {
     }
   });
 
+
+
+  // 2. 给表单注册一个表单校验成功的事件, 成功的时候, 阻止默认的表单提交, 使用 ajax 提交
+  $('#form').on("success.form.bv", function( e ) {
+    e.preventDefault();
+
+
+    // 发送 ajax 请求, 进行登录请求
+    // 如果 后台在响应头中, 配置了类型为 application/json,
+    // jQuery 会自动将响应结果, 当成 json字符串解析, 所以 dataType 可以省略
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      data: $("#form").serialize(),
+      dataType: "json",
+      success: function( data ) {
+        console.log( data );
+
+        if ( data.success ) {
+          console.log( "登录成功" );
+        }
+
+        if ( data.error === 1001 ) {
+          console.log( "密码错误" );
+        }
+
+        if (data.error === 1000 ) {
+          console.log( "用户名不存在" );
+        }
+      }
+    })
+  })
+
 })
