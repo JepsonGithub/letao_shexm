a00ad690076c66e457e75a4c461d7e77a4f0c3d1
jepsongithub <jepsonpp@qq.com>
2018-04-06 01:10:17 +0800

    common: 进度条功能完成

---
+++ public/back/js/common.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Jepson on 2018/4/6.
+ */
+
+
+// 禁用小圆环
+NProgress.configure({ showSpinner: false });
+
+//// 开启进度条
+//NProgress.start();
+//
+//// 关闭进度条
+//setTimeout(function() {
+//  NProgress.done();
+//}, 2000)
+
+// ajax 开始了
+$(document).ajaxStart(function() {
+  console.log( "ajax开始了" )
+  NProgress.start();
+})
+
+// ajax 结束了
+$(document).ajaxStop(function() {
+  setTimeout(function() {
+    console.log( "ajax结束了" )
+    NProgress.done();
+  }, 500);
+})
