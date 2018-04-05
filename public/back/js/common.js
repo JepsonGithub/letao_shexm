3406493b2d6d0f252a6e1669abed52f509b4b748
jepsongithub <jepsonpp@qq.com>
2018-04-06 02:44:16 +0800

    index: 登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -30,21 +30,54 @@ $(document).ajaxStop(function() {
 
 
 
-// 二级菜单切换功能
-$(".category").click(function() {
-  $(this).next().stop().slideToggle();
+$(function() {
+
+  // 二级菜单切换功能
+  $(".category").click(function() {
+    $(this).next().stop().slideToggle();
+  })
+
+
+  // 菜单栏切换功能
+  $('.icon_menu').click(function() {
+    $('.lt_aside').toggleClass("menuhide");
+    $('.lt_topbar').toggleClass("menuhide");
+    $('.lt_main').toggleClass("menuhide");
+  });
+
+
+  // 显示退出模态框
+  $('.icon_logout').click(function() {
+    $('#logoutModal').modal("show");
+  });
+
+
+  // 退出功能
+  $('#logoutBtn').click(function() {
+    $.ajax({
+      url: "/employee/employeeLogout",
+      type: "get",
+      success: function( data ) {
+        if ( data.success ) {
+          location.href = "login.html";
+        }
+      }
+    })
+  });
+
+
+  // 登录拦截, 如果地址栏里面没有 login.html, 说明需要判断登录状态
+  if ( location.href.indexOf("login.html") === -1 ) {
+    // 发送 ajax, 请求查询管理员是否登录
+    $.ajax({
+      type: "get",
+      url: "/employee/checkRootLogin",
+      success: function( data ) {
+        console.log( data );
+        if ( data.error ) {
+          location.href = "login.html";
+        }
+      }
+    })
+  }
 })
-
-
-// 菜单栏切换功能
-$('.icon_menu').click(function() {
-  $('.lt_aside').toggleClass("menuhide");
-  $('.lt_topbar').toggleClass("menuhide");
-  $('.lt_main').toggleClass("menuhide");
-});
-
-
-// 显示退出模态框
-$('.icon_logout').click(function() {
-  $('#logoutModal').modal("show");
-});
